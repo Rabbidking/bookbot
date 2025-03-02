593f7d144d02fab8ad89ead38d4ba34eb0c610a2
Rabbidking <bickel.john@yahoo.com>
2025-03-02 10:27:15 -0500

    Edited code to match current project guidelines

--- main.py
+++ main.py
@@ -1,47 +1,37 @@
-from stats import get_num_words
+import sys
+from stats import (
+    get_num_words, 
+    get_chars_dict, 
+    sort_chars_to_list
+)
+
+if len(sys.argv) < 2:
+    raise Exception("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
 def main():
-    book_path = "books/frankenstein.txt"
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    char_dictionary = get_chars_in_string(text)
+    char_dictionary = get_chars_dict(text)
     char_sorted = sort_chars_to_list(char_dictionary)
 
-    print(f"--- Begin report of {book_path} ---")
-    print(f"{num_words} words found in the document")
-    print()
-
-    for item in char_sorted:
-        if not item["char"].isalpha():
-            continue
-        print(f"The '{item['char']}' character was found {item['num']} times")
-
-    print("--- End report ---")
+    print_report(book_path, num_words, char_sorted)
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
     
-
-
-def get_chars_in_string(text):
-    chars = {}
-    for i in text:
-        lowercase = i.lower()
-        if lowercase in chars:
-            chars[lowercase] += 1
-        else:
-            chars[lowercase] = 1
-    return chars
-
-def sort_chars_to_list(char_dict):
-    sorted = []
-    for i in char_dict:
-        sorted.append({"char": i, "num": char_dict[i]})
-    sorted.sort(reverse = True, key = sort_on)
-    return sorted
-
-def sort_on(i):
-    return i["num"]
+def print_report(book_path, num_words, char_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in char_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,23 @@
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+def sort_chars_to_list(char_dict):
+    sorted = []
+    for i in char_dict:
+        sorted.append({"char": i, "num": char_dict[i]})
+    sorted.sort(reverse = True, key = sort_on)
+    return sorted
+
+def sort_on(i):
+    return i["num"]
